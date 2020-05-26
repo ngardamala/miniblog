f1aad182d75b4574079dd43aeb430a626ff5ea80
Nikolay Gardamala <nga@ciklum.com>
2020-05-26 15:20:26 +0300

    Added option to run with gunicorn

--- manage.py
+++ manage.py
@@ -1,12 +1,67 @@
 #!/usr/bin/env python
 import os
-from flask_script import Manager
+from flask_script import Manager, Command, Option
 
 from app import create_app
+
+
+class GunicornServer(Command):
+
+    description = 'Run the app within Gunicorn'
+
+    def __init__(self, host='127.0.0.1', port=8000, workers=4):
+        self.port = port
+        self.host = host
+        self.workers = workers
+
+    def get_options(self):
+        return (
+            Option('-H', '--host',
+                   dest='host',
+                   default=self.host),
+
+            Option('-p', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+
+            Option('-w', '--workers',
+                   dest='workers',
+                   type=int,
+                   default=self.workers),
+        )
+
+    def __call__(self, app, host, port, workers):
+
+        from gunicorn import version_info
+
+        if version_info < (0, 9, 0):
+            from gunicorn.arbiter import Arbiter
+            from gunicorn.config import Config
+            arbiter = Arbiter(Config({'bind': "%s:%d" % (host, int(port)),
+                                     'workers': workers}), app)
+            arbiter.run()
+        else:
+            from gunicorn.app.base import Application
+
+            class FlaskApplication(Application):
+                def init(self, parser, opts, args):
+                    return {
+                        'bind': '{0}:{1}'.format(host, port),
+                        'workers': workers
+                    }
+
+                def load(self):
+                    return app
+
+            FlaskApplication().run()
+
 
 app = create_app()
 app.config.from_object(os.environ['APP_SETTINGS'])
 manager = Manager(app)
+manager.add_command("gunicorn", GunicornServer())
+
 
 if __name__ == '__main__':
     manager.run()
